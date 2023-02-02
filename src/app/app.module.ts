91f40070bd65bf742607eed0079548389cf0b600
rene-marcker <rene-marcker@hotmail.com>
2023-02-02 09:54:36 +0100

    feat: generated trainer page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LandingPage } from './pages/landing/landing.page';
+import { TrainerPage } from './pages/trainer/trainer.page';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPage
+    LandingPage,
+    TrainerPage
   ],
   imports: [
     BrowserModule
