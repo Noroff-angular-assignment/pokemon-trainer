cb3c471fefcb7f349e489be9eaefe1870407a0c9
rene-marcker <rene-marcker@hotmail.com>
2023-02-02 09:53:32 +0100

    feat: generated the landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingPage } from './pages/landing/landing.page';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPage
   ],
   imports: [
     BrowserModule
