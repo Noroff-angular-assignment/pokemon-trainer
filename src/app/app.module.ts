d65e501529e9984a08670e50a07665e83155f1b0
Michael Piepgras Neergaard <michael@neergaard.dk>
2023-02-02 20:21:06 +0100

    feat: created user and user service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
