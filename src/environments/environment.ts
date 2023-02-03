304c6f8aaa1f48d505399edad15f1a71c4e1618f
rene-marcker <rene-marcker@hotmail.com>
2023-02-03 15:19:22 +0100

    feat: added environment file to git OBS!! NEED WORK AROUND

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    APITrainers: "https://immediate-halved-nickel.glitch.me/trainers",
+    APIPokemon: "https://pokeapi.co/api/v2/",
+    APIKey: "MyNZLtBYMvDEs7EsrsLn1fa2d1QTlEJswKHTUzka94Arf4Ctji9STEjXTEesAjrb"
+};
